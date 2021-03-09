65b2eadf6abc8056fedef6c6833cfe3047aefcda
ArthurBJ <baybikov.artur@mail.ru>
2021-03-09 23:29:12 +0300

    final

--- 1/index.php
+++ 1/index.php
@@ -1,60 +1,65 @@
 <?php
+if (isset($_REQUEST["send"])) {
 
-$code = $_REQUEST["code"];
-$params = $_REQUEST["params"];
-$array = array(0);
-$cell = 0;
+    $code = $_REQUEST["code"];
+    $params = $_REQUEST["params"];
+    $array = array(0);
+    $cell = 0;
 
-$codeArray = str_split($code);
-$paramsArray = str_split($params);
-$index = 0;
-$brackets = 0;
+    $codeArray = str_split($code);
+    $paramsArray = str_split($params);
+    $index = 0;
+    $brackets = 0;
 
-for ($i = 0; $i < count($codeArray); $i++) {
-    switch ($codeArray[$i]) {
-        case "+":
-            $array[$cell]++;
-            break;
-        case "-":
-            $array[$cell]--;
-            break;
-        case ".":
-            print chr($array[$cell]);
-            break;
-        case ",":
-            $array[$cell] = ord($paramsArray[$index++]);
-            break;
-        case ">":
-            $cell++;
-            break;
-        case "<":
-            $cell--;
-            break;
-        case "[":
-            if (!$array[$cell]) {
-                $brackets = 1;
-                while ($brackets) {
-                    $i++;
-                    if ($codeArray[$i] == "[") {
-                        $brackets++;
-                    } elseif ($codeArray[$i] == "]") {
-                        $brackets--;
+    for ($i = 0; $i < count($codeArray); $i++) {
+        switch ($codeArray[$i]) {
+            case "+":
+                $array[$cell]++;
+                break;
+            case "-":
+                $array[$cell]--;
+                break;
+            case ".":
+                print chr($array[$cell]);
+                break;
+            case ",":
+                $array[$cell] = ord($paramsArray[$index++]);
+                break;
+            case ">":
+                $cell++;
+                break;
+            case "<":
+                $cell--;
+                break;
+            case "[":
+                if (!$array[$cell]) {
+                    $brackets = 1;
+                    while ($brackets) {
+                        $i++;
+                        if ($codeArray[$i] == "[") {
+                            $brackets++;
+                        } elseif ($codeArray[$i] == "]") {
+                            $brackets--;
+                        }
                     }
                 }
-            }
-            break;
-        case "]":
-            if ($array[$cell]) {
-                $brackets = 1;
-                while ($brackets) {
-                    $i--;
-                    if ($codeArray == "]") {
-                        $brackets++;
-                    } elseif ($codeArray[$i] == "[") {
-                        $brackets--;
+                break;
+            case "]":
+                if ($array[$cell]) {
+                    $brackets = 1;
+                    while ($brackets) {
+                        $i--;
+                        if ($codeArray == "]") {
+                            $brackets++;
+                        } elseif ($codeArray[$i] == "[") {
+                            $brackets--;
+                        }
                     }
                 }
-            }
-            break;
+                break;
+        }
     }
+} else {
+    include "index.html";
 }
+?>
