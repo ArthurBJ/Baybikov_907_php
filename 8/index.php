d3fccc77819f87f6abdf04cb0bf768f82e49eb7a
ArthurBJ <baybikov.artur@mail.ru>
2021-05-05 17:10:00 +0300

    calendar fix

--- 8/index.php
+++ 8/index.php
@@ -1,66 +1,72 @@
 <?php
 if (isset($_POST['send'])) {
-    function calendar($month, $year) {
-        $months = array(
-            1 => 'Январь',
-            2 => 'Февраль',
-            3 => 'Март',
-            4 => 'Апрель',
-            5 => 'Май',
-            6 => 'Июнь',
-            7 => 'Июль',
-            8 => 'Август',
-            9 => 'Сентябрь',
-            10 => 'Октябрь',
-            11 => 'Ноябрь',
-            12 => 'Декабрь'
-        );
+    if (isset($_POST['month'])) {
+        $month = $_POST['month'];
+    } else $month = date('m', time());
 
-        $out = '<div class="calendar-item">
-                    <div>'.$months[$month].' '.$year.'</div>
-                    <table>
-                    <tr>
-                        <th>ПН</th>
-                        <th>Вт</th>
-                        <th>Ср</th>
-                        <th>Чт</th>
-                        <th>Пт</th>
-                        <th>Сб</th>
-                        <th>Вс</th>
-                    </tr>';
+    $months = array(
+        1 => 'Январь',
+        2 => 'Февраль',
+        3 => 'Март',
+        4 => 'Апрель',
+        5 => 'Май',
+        6 => 'Июнь',
+        7 => 'Июль',
+        8 => 'Август',
+        9 => 'Сентябрь',
+        10 => 'Октябрь',
+        11 => 'Ноябрь',
+        12 => 'Декабрь'
+    );
 
-        $day_week = date('N', mktime(0,0,0,$month,1,$year));
-        $day_week--;
+    $key = $month;
+    if (gettype($key) != 'boolean') {
+        $key = $month;
+    } else {
+        $key = new DateTime('now', new DateTimeZone('Europe/Moscow'));
+        $key = $key->format('n');
+    }
+    $dayofmonth = cal_days_in_month(CAL_GREGORIAN, $key, 2021);
+    $num = 0;
+    $day_count = 1;
 
-        $out .= '<tr>';
-
-        for ($x = 0; $x < $day_week; $x++) {
-            $out .= '<td></td>';
+    for ($i = 0; $i < 7; $i++) {
+        $dayofweek = date('w', mktime(0, 0, 0, date($key), $day_count, date('Y')));
+        $dayofweek = $dayofweek - 1;
+        if ($dayofweek == -1) $dayofweek = 6;
+        if ($dayofweek == $i) {
+            $week[$num][$i] = $day_count;
+            $day_count++;
+        } else {
+            $week[$num][$i] = "";
         }
-
-        $days_count = 0;
-        $days_month = date('t', mktime(0,0,0,$month,1,$year));
-
-        for ($day = 1; $day <= $days_month; $day++) {
-            $out .= '<td>' . $day . '</td>';
-
-            if ($day_week == 6) {
-                $out .= '</tr>';
-                if (($days_count + 1) != $days_month ) {
-                    $out .= '<tr>';
-                }
-                $day_week = -1;
-            }
-
-            $day_week++;
-            $days_count++;
+    }
+    while (true) {
+        $num++;
+        for ($i = 0; $i < 7; $i++) {
+            $week[$num][$i] = $day_count;
+            $day_count++;
+            if ($day_count > $dayofmonth) break;
         }
-
-        $out .= '</tr></table></div>';
-        return $out;
+        if ($day_count > $dayofmonth) break;
     }
 
-    echo calendar($_POST['month'], date('Y'));
+    $now = date('d', time());
+
+    echo "<div class='calendar-item'><table border=1 style='text-align: center;>";
+    echo "<div'>" . $months[$key] . "</div>";
+    echo "<tr><td>Пн</td><td>Вт</td><td>Ср</td><td>Чт</td><td>Пт</td><td>Сб</td><td>Вс</td><tr>";
+    for ($i = 0; $i < count($week); $i++) {
+        for ($j = 0; $j < 7; $j++) {
+            if (!empty($week[$i][$j])) {
+                if (($week[$i][$j] == $now))
+                    echo "<td style='color: blue'>" . $now . "</td>";
+                else echo "<td>" . $week[$i][$j] . "</td>";
+            } else echo "<td>&nbsp;</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table></div>";
     echo "<link rel='stylesheet' href='style.css'>";
 } else {
     include "index.html";
